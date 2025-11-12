e8ccbae56466c22288b8fa1536d1b2c8250e9129
ManelEsquivel <157485831+ManelEsquivel@users.noreply.github.com>
2025-11-12 10:57:07 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '40px' }}>
+      <h1>Bienvenido a la Boda de Manel y Carla 💍</h1>
+      <p>
+        Accede al <a href="/bot_boda_asistente">Asistente de Boda</a>
+      </p>
+    </main>
+  );
+}
