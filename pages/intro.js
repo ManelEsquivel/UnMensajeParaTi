15c287031a1fe8f86cf14280552dfdd511917dde
ManelEsquivel <157485831+ManelEsquivel@users.noreply.github.com>
2025-11-18 16:31:19 +0100

    Update intro.js

--- pages/intro.js
+++ pages/intro.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'; 
 
 export default function IntroPage() {
   const router = useRouter();
