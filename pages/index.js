4f335498b36a954db11cfdb2c30947c572a067e3
ManelEsquivel <157485831+ManelEsquivel@users.noreply.github.com>
2025-11-19 15:33:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
         {/* --- ETIQUETAS OPEN GRAPH MODIFICADAS (SOLUCIÓN CACHÉ) --- */}
         <meta property="og:title" content="Tenemos un mensaje muy importante para ti" />
         <meta property="og:description" content="Hemos pensado en ti para que guardes una fecha muy especial." />
-        <meta property="og:image" content="/boda_icon_3.png?v=2" /> {/* CORREGIDO A .png y añadido ?v=2 */}
+        <meta property="og:image" content="/boda_icon_3.JPG?v=3" /> {/* RUTA ACTUALIZADA y añadido ?v=3 */}
         <meta property="og:type" content="website" />
         {/* -------------------------------------------------------- */}
         
