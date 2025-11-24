4958c70bd72c5fa24db13704073e815364e4f5f0
ManelEsquivel <157485831+ManelEsquivel@users.noreply.github.com>
2025-11-24 12:31:59 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,9 +6,8 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
-        // Esto le dice a Next.js: "Solo optimiza fotos de MI bucket de boda", 
-        // evitando errores de seguridad.
-        pathname: '/v0/b/boda-74934.firebasestorage.app/o/**',
+        port: '',
+        pathname: '/**', // <--- Aceptamos CUALQUIER ruta dentro de Firebase
       },
     ],
   },
